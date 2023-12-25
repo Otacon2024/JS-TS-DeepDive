657fca651f81092521d42b035da4192f0eed7e69
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-26 00:24:18 +0100

    Add const variable to index.js

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,11 +1,17 @@
-let surname = "Doe";
-console.log(surname);
-
 // Variable name cannot be a reserved keyword
 // Should be meaningful
 // Cannot start with a number (1name)
 // Cannot contain a space or hyphen (-)
 // Are case-sensitive
 
+let surname = "Doe";
+console.log(surname);
+
 let firstName = "John";
 let lastName = "Smith";
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+const interestRate2 = 0.3;
