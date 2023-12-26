8b4f61ad04d6cd95550344108de5bd0cc6b52608
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-26 23:49:46 +0100

    Add function taking a single parameter

--- js-basics/index.js
+++ js-basics/index.js
@@ -3,18 +3,22 @@ let person = {
   name: "Akira",
   age: 20,
 };
-
 // Dot Notation
 person.name = "Mary";
-
 // Bracket Notation
 let selection = "name";
 person[selection] = "John";
 
 // Array
 let selectedColors = ["red", "blue"];
-selectedColors[2] = "green";
 selectedColors[3] = 1;
-
-console.log(selectedColors);
 console.log(selectedColors.length);
+
+// Function
+function greet(name) {
+  console.log("Hello " + name);
+}
+greet("C17");
+
+let greet2 = (name) => console.log("Hello " + name);
+greet2("C18");
