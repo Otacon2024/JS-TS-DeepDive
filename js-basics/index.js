16502e3644dfab9e25645a8d8dff1312cc6cc618
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-26 23:19:30 +0100

    Add object + use "dot" and "braket" notation to access a property

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,35 +1,14 @@
-// Variable name cannot be a reserved keyword
-// Should be meaningful
-// Cannot start with a number (1name)
-// Cannot contain a space or hyphen (-)
-// Are case-sensitive
+let person = {
+  name: "Akira",
+  age: 20,
+};
 
-let surname = "Doe";
-console.log(surname);
+// Dot Notation
+person.name = "Mary";
 
-let firstName = "John";
-let lastName = "Smith";
+// Bracket Notation
+let selection = "name";
+person[selection] = "John";
 
-let interestRate = 0.3;
-interestRate = 1;
-console.log(interestRate);
-
-const interestRate2 = 0.3;
-
-// Primitive/Value Types
-// String, Number, Boolean, undefined, null
-
-let name = "Akira"; // String Literal
-let age = 20; // Number Literal
-let isApproved = false; // Boolean Literal
-let firstName2 = undefined;
-
-let selectedColor = null;
-//selectedColor = "red";
-
-console.log(typeof name);
-console.log(typeof age);
-console.log(typeof isApproved);
-
-console.log(typeof firstName2);
-console.log(typeof selectedColor);
+console.log(person);
+console.log(person.name);
