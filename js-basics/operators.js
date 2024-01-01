eafb5a755f56cabb0b42a9733e0d70f1af93f310
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2024-01-01 23:31:59 +0100

    Add ++, +=, *=, /=, -= arithmetic "shorthand" operators

--- js-basics/operators.js
+++ js-basics/operators.js
@@ -1,14 +1,14 @@
 // Arithmetic operators
-
+/*
 let x = 10;
 let y = 3;
 
-// console.log(x + y);
-// console.log(x - y);
-// console.log(x * y);
-// console.log(x / y);
-// console.log(x % y); // Remainder of division
-// console.log(x ** y); // Exponentiation
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y); // Remainder of division
+console.log(x ** y); // Exponentiation
 
 // Increment (++)
 console.log(x); // 10
@@ -19,3 +19,20 @@ console.log(x); // 12
 // Decrement (--)
 console.log(--x); // 11
 console.log(x--); // 11 (because it display the value of x before decrementing)
+*/
+
+// Assignment operators
+
+let x = 9;
+
+x++;
+x = x + 1;
+
+x = x + 5;
+x += 5;
+
+x = x * 3;
+x *= 3;
+
+x /= 3;
+x -= 3;
