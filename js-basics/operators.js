4e5d6cb2e6441e6ce82f1c3030ea034c29dfbdaf
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2024-01-01 22:58:55 +0100

    Add arithmetic operators to operators.js

---
+++ js-basics/operators.js
@@ -0,0 +1,11 @@
+// Arithmetic operators
+
+let x = 10;
+let y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y); // Remainder of division
+console.log(x ** y); // Exponentiation
